82b88d58c20d2002422d8de9c866b88df536c78d
Josue Morales <157775085+PR2014FL@users.noreply.github.com>
2024-04-19 10:34:27 -0400

    Created view called 'josue'. Renamed 'public' directory to 'views'. Removed css file and incorporated styling into <head> of html files.

---
+++ README.md
@@ -0,0 +1,9 @@
+# SBA 318 - Express Server Application
+
+## Objectives
+- Create a server application with Node and Express.
+- Create a RESTful API using Express.
+- Create Express middleware.
+- Use Express middleware.
+- Use a template engine to render views with Express.
+- Interact with a self-made API through HTML forms.
--- server.js
+++ server.js
@@ -6,20 +6,47 @@ const profilePics = require("./data/userProfilePics");
 const bodyParser = require("body-parser");
 const app = express();
 const port = process.env.PORT || 3000;
+const fs = require("fs");
 
 __dirname = path.dirname(require.main.filename);
 
-function logReq(req, res, next) {
-  console.log(`Request initiated at ${req.url}`);
-  next();
-}
-app.use(logReq);
+app.use(logReq); //logging middleware for requests and responses info
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json({ extended: true }));
 
+app.engine("josue", (filePath, options, callback) => {
+  // template engine
+  fs.readFile(filePath, (err, content) => {
+    if (err) return callback(err);
+
+    const rendered = content
+      .toString()
+      .replaceAll("#title#", `${options.title}`)
+      .replace("#content#", `${options.content}`);
+    return callback(null, rendered);
+  });
+});
+
+app.set("views", "./views"); // specify directory
+app.set("view engine", "josue"); // register the template engine
+
+app.get("/info", (req, res) => {
+  const options = {
+    title: "Rendering Views",
+    content: `Here, I've created a basic express template engine using <code>app.engine()</code>
+      and the <code>fs</code> module, then used <code>res.render</code> to
+      render this page using custom content within the template.`,
+    // "Here, I've created a basic template engine using <code>app.engine()</code> \//the slashes didn't work here it caused my ap to crash
+    // and the <code>fs</code> module, then used <code>res.render</code> to \
+    // render this page using custom content within the template. \",
+  };
+
+  res.render("index", options);
+});
+
 app.route("/").get((req, res) => {
-  res.sendFile(__dirname + "/public/index.html");
+  res.sendFile(__dirname + "/views/index.html");
 });
 
 app.route("/api/users").get((req, res) => {
@@ -33,6 +60,13 @@ app.route("/api/ProfilePics").get((req, res) => {
 app.route("/api/Comments").get((req, res) => {
   res.send(comments);
 });
+
+function logReq(req, res, next) {
+  //Logging Middleware function needs to be down here for the response logging to occur as a synchronous action
+  console.log(`Request initiated: ${req.method} ${req.url}`);
+  next();
+  console.log(`Outgoing Response: ${res.statusCode} ${res.statusMessage}`);
+}
 
 app.listen(port, () => {
   console.log(`Server Running in Port:${port}`);
